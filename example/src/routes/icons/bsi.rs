dd47321483e2cb7c597dce2105cd11092441fe94
Jonathan Gao <gsmlg.com@gmail.com>
2023-01-27 23:51:46 +0800

    fix: Fix example icons style.

--- example/src/routes/icons/bsi.rs
+++ example/src/routes/icons/bsi.rs
@@ -11,14 +11,24 @@ use yew_duskmoon_icons::bsi::*;
 pub fn component() -> Html {
   let style = use_style(css!(
     r#"
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    gap: 2rem;
-    list-style: none;
-    width: 100%;
-    padding: 0;
+    width: 83.4%;
+    .icon-container {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      align-items: flex-start;
+      gap: 2rem;
+      width: 100%;
+      padding: 0;
+    }
+    .icon {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 140px;
+    }
     .t::after {
       content: ":";
       display: inline-flex;
@@ -33,18 +43,27 @@ pub fn component() -> Html {
   html! {
     <div class="app">
       <div class="app-main">
-        <Card title={ html!{
-          <Typography level={TypographyLevel::H1}>
-          {"Duskmoon Icons - Bootstrap Icons"}
-          </Typography>
-        }}>
-          <div class={ style }>
+        <Card 
+          classes={ style }
+          title={html!{
+            <Typography level={TypographyLevel::H1}>
+              {"Duskmoon Icons - Bootstrap Icons"}
+            </Typography>
+          }}
+        >
+          <div class="icon-container">
             {BSI_NAMES.into_iter().map(|n| {
               html!{
                 <div class="icon">
                   <label class="t">{ format!("{}", n) }</label>
                   <div class="v">
-                    <BSIcon name={n} />
+                    <BSIcon name={n} size={32} />
+                  </div>
+                  <div class="c">
+                    {format!("html!{{ <BS_{} /> }}", n)}
+                  </div>
+                  <div class="c">
+                    {format!("html!{{ <BSIcon name=\"{}\" /> }}", n)}
                   </div>
                 </div>
               }
--- example/src/routes/icons/mdi.rs
+++ example/src/routes/icons/mdi.rs
@@ -11,21 +11,28 @@ use yew_duskmoon_icons::mdi::*;
 pub fn component() -> Html {
   let style = use_style(css!(
     r#"
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    gap: 2rem;
-    list-style: none;
-    width: 100%;
-    padding: 0;
-    .t::after {
-      content: ":";
-      display: inline-flex;
-      padding: 0 0.1em;
+    width: 83.4%;
+    .icon-container {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      align-items: flex-start;
+      gap: 2rem;
+      width: 100%;
+      padding: 0;
+    }
+    .icon {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 140px;
     }
     .t,.v,.c {
       display: flex;
+      justify-content: center;
+      align-items: center;
     }
   "#
   ));
@@ -33,18 +40,27 @@ pub fn component() -> Html {
   html! {
     <div class="app">
       <div class="app-main">
-        <Card title={ html!{
-          <Typography level={TypographyLevel::H1}>
-          {"Duskmoon Icons - Material Design Icons"}
-          </Typography>
-        }}>
-          <div class={ style }>
+        <Card
+          classes={ style }
+          title={ html!{
+            <Typography level={TypographyLevel::H1}>
+              {"Duskmoon Icons - Material Design Icons"}
+            </Typography>
+          }}
+        >
+          <div class="icon-container">
             {MDI_NAMES.into_iter().map(|n| {
               html!{
                 <div class="icon">
                   <label class="t">{ format!("{}", n) }</label>
                   <div class="v">
-                    <MDIcon name={n} />
+                    <MDIcon name={n} size={32} />
+                  </div>
+                  <div class="c">
+                    {format!("html!{{ <MD_{} /> }}", n)}
+                  </div>
+                  <div class="c">
+                    {format!("html!{{ <MDIcon name=\"{}\" /> }}", n)}
                   </div>
                 </div>
               }
